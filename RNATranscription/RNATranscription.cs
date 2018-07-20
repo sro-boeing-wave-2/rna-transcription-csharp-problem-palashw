36da4ce9fb70b1aa51970088e637b7d3aaa3f8bc
Palash <palashw173@gmail.com>
2018-07-20 10:11:05 +0530

    RNA transcription done

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,36 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //char[] ntide = nucleotide.ToCharArray();
+            string rna = "";
+            foreach (char i in nucleotide)
+            //for(int i = 0; i< ntide.Length; i++)
+            {
+                switch (i)
+                {
+                    case 'G':
+                        { rna += "C".ToString();
+                            break;
+                        }
+                    case 'A':
+                        {
+                            rna += "U".ToString();
+                            break;
+                        }
+                    case 'C':
+                        {
+                            rna += "G".ToString();
+                            break;
+                        }
+                    case 'T':
+                        {
+                            rna += "A".ToString();
+                            break;
+                        }
+                }
+            }
+
+            return rna;
         }
     }
 }
